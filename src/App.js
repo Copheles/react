0309b8e20eef72d4403f02dfe429d2e69cd0a9bf
Copheles <xixxykitkit@gmail.com>
2022-11-01 16:59:36 +0630

    about go to props

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Employee from "./components/Employee";
 
 function App() {
+  
+  const showEmployees = false;
   return (
     <div className="App">
-      
+      {showEmployees ? <Employee /> : <p>You cannot see the employees</p>}
     </div>
   );
 }
---
+++ src/components/Employee.js
@@ -0,0 +1,10 @@
+
+
+function Employee(){
+  return(
+    <h3>Here is an employee</h3>
+  )
+
+}
+
+export default Employee;
