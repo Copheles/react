1dfa04e66061ec75ba33637765a480e2aec196af
Copheles <xixxykitkit@gmail.com>
2022-11-04 21:02:25 +0630

    pass component as a props

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import "./index.css";
 import Employee from "./components/Employee";
 import { useState } from "react";
-import { v4 as uuidv4 } from "uuid";
+import AddEmployee from "./components/AddEmployee";
+import EditEmployee from "./components/EditEmployee";
 
 function App() {
   const [role, setRole] = useState("");
@@ -42,20 +43,31 @@ function App() {
       role: "Developer",
       img: "https://images.pexels.com/photos/775358/pexels-photo-775358.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
     },
-  
   ]);
 
   function updateEmployee(id, newName, newRole) {
     const updateEmployees = employees.map((employee) => {
-      if(id === employee.id){
-        return{...employee, name: newName, role: newRole}
+      if (id === employee.id) {
+        return { ...employee, name: newName, role: newRole };
       }
-      return employee
+      return employee;
     });
-    setEmployees(updateEmployees)
+    setEmployees(updateEmployees);
+  }
+
+  function newEmployee(name, role, img) {
+    const newEmployee = {
+      id: employees.length + 1,
+      name,
+      role,
+      img,
+    };
+
+    setEmployees([...employees, newEmployee]);
   }
 
   const showEmployees = true;
+
   return (
     <div className="App">
       {showEmployees ? (
@@ -68,17 +80,28 @@ function App() {
             }}
           />
           <div className="flex flex-wrap justify-center">
-            {employees.map((employee, idx) => (
-              <Employee
-                key={employee.id}
-                id={employee.id}
-                name={employee.name}
-                role={employee.role}
-                img={employee.img}
-                updateEmployee={updateEmployee}
-              />
-            ))}
+            {employees.map((employee, idx) => {
+              const editEmployee = (
+                <EditEmployee
+                  id={employee.id}
+                  name={employee.name}
+                  role={employee.role}
+                  updateEmployee={updateEmployee}
+                />
+              );
+              return (
+                <Employee
+                  key={employee.id}
+                  id={employee.id}
+                  name={employee.name}
+                  role={employee.role}
+                  img={employee.img}
+                  editEmployee={editEmployee}
+                />
+              );
+            })}
           </div>
+          <AddEmployee newEmployee={newEmployee} />
         </>
       ) : (
         <p>You cannot see the employees</p>
